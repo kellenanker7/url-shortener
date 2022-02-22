2782696a6c46bd180d4336808ae0841d491f75f6
KELLEN ANKER <kellenanker7@gmail.com>
2022-02-21 21:33:36 -0500

    checkpoint

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import logging
+import boto3
 
 from aws_lambda_powertools.logging import Logger
 from aws_lambda_powertools.event_handler.api_gateway import (
     ApiGatewayResolver,
     ProxyEventType,
-    Response,
 )
 from aws_lambda_powertools.event_handler.exceptions import (
     BadRequestError,
@@ -14,16 +14,65 @@ from aws_lambda_powertools.event_handler.exceptions import (
     UnauthorizedError,
 )
 
+ddb_table = "url-shortener"
 
 logger = Logger(level=logging.INFO)
 app = ApiGatewayResolver(proxy_type=ProxyEventType.APIGatewayProxyEventV2)
 
+ddb_client = boto3.client("dynamodb")
 
-@app.get("/<id>")
-def redirect(id):
-    long_url = "https://google.com" # Look up real URL in DB based on short URL
 
-    # Simple redirect to real URL
+def gen_id(long_url):
+    short_url_id = hash(long_url)  # Do things to generate id
+    logger.info(short_url_id)
+
+    resp = ddb_client.get_item(
+        TableName=ddb_table,
+        Key={
+            "ShortUrlId": {"S": str(short_url_id)},
+        },
+    )
+
+    logger.info(resp)
+
+    if resp == "":
+        ddb_client.put_item(
+            TableName=ddb_table,
+            Item={
+                "ShortUrlId": {"S": short_url_id},
+                "LongUrl": long_url,
+                # "TTL": somevalue,
+            },
+        )
+
+    return short_url_id
+
+
+@app.get("/shorten")
+def shorten():
+    long_url = app.current_event.get_query_string_value(
+        name="longUrl", default_value=""
+    )
+    if long_url == "":
+        return BadRequestError("Missing longUrl query parameter")
+
+    return {"id": gen_id(long_url)}
+
+
+@app.get("/s/<sid>")
+def redirect(sid):
+    long_url = ddb_client.get_item(
+        TableName=ddb_table,
+        Key={
+            "ShortUrlId": {"S": short_url_id},
+        },
+    )
+
+    logger.info(long_url)
+
+    if long_url == "":
+        return BadRequestError("Invalid short URL. Has that short URL expired?")
+
     return Response(
         status_code=302,
         body="redirecting...",
@@ -32,6 +81,10 @@ def redirect(id):
     )
 
 
+@app.get("/status")
+def status():
+    return {"status": "all good"}
+
+
 def api_handler(event, context):
-    logger.info(event)
     return app.resolve(event, context)
