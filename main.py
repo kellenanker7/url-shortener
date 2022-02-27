e73780cae63399b9ec2f95b2d2ea36e9073ad416
KELLEN ANKER <kellenanker7@gmail.com>
2022-02-27 15:04:01 -0500

    checkpoint

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import logging
+import random
 import boto3
+import time
 import json
 import sys
 import os
@@ -22,9 +24,9 @@ logger = Logger(level=logging.INFO)
 app = ApiGatewayResolver(proxy_type=ProxyEventType.APIGatewayProxyEventV2)
 
 dynamodb = boto3.resource("dynamodb")
-table = dynamodb.Table("url-shortener")
-
+table = dynamodb.Table(os.environ.get("DDB_TABLE"))
 domain_name = os.environ.get("DOMAIN_NAME")
+
 chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 base = len(chars)
 
@@ -54,25 +56,29 @@ def shorten():
         logger.warning("No protocol found in 'longUrl' - defaulting to 'https://'")
         long_url = "https://" + long_url
 
-    url_hash = hash(long_url)
-    if url_hash < 0:
-        url_hash += sys.maxsize + 1  # Must be positive
-    suid = encode(url_hash)
+    # encode(550731776) = baaaaa (smallest input that generates output length 6)
+    # encode(30840979455) = 999999 (largest input that generates output length 6)
+    # 30,840,979,455 - 550,731,776 = 30,290,247,679 < 62^6 = 56,800,235,584
+    # So, for encode() the number of possible inputs < num of possible outputs
+    # We should never hit any collisions. We're also expiring items after 365 days.
+    # I hope we don't generate 56,800,235,584 short URLs every year :)
+    uid = random.randint(550731776, 30840979455)
+    suid = encode(uid)
 
     try:
+        now = int(time.time())
         table.put_item(
-            Item={"ShortUrlId": str(suid), "LongUrl": long_url, "Clicks": 0},
-            ConditionExpression="attribute_not_exists(ShortUrlId)",
+            Item={
+                "ShortUrlId": str(suid),
+                "LongUrl": long_url,
+                "Clicks": 0,
+                "Timestamp": now,
+                "TTL": now + (365 * 24 * 60 * 60),  # one year from now
+            },
         )
-    except ClientError as e:
-        if e.response["Error"]["Code"] == "ConditionalCheckFailedException":
-            pass
-        else:
-            logger.error(e)
-            raise BadRequestError("Bad PutItem request")
     except Exception as e:
         logger.error(e)
-        raise InternalServerError("Unexpected error during UpdateItem")
+        raise InternalServerError("Unexpected error during PutItem")
 
     logger.info(f"{suid}: {long_url}")
     return {"short_url": f"https://{domain_name}/{suid}"}
@@ -81,17 +87,22 @@ def shorten():
 @app.get("/<suid>")
 def redirect(suid):
     try:
-        item = table.update_item(
-            Key={
-                "ShortUrlId": str(suid),
-            },
+        # https://stackoverflow.com/a/60064828
+        item = table.query(
+            KeyConditionExpression="#ShortUrlId = :val",
+            ExpressionAttributeNames={"#ShortUrlId": "ShortUrlId"},
+            ExpressionAttributeValues={":val": str(suid)},
+            Limit=1,
+        )["Items"][0]
+
+        long_url = item["LongUrl"]
+        table.update_item(
+            Key={"ShortUrlId": str(suid), "Timestamp": item["Timestamp"]},
             ConditionExpression="attribute_exists(ShortUrlId)",
             UpdateExpression="SET #Clicks = #Clicks + :val",
             ExpressionAttributeNames={"#Clicks": "Clicks"},
             ExpressionAttributeValues={":val": 1},
-            ReturnValues="ALL_NEW",
         )
-        long_url = item["Attributes"]["LongUrl"]
 
         return Response(
             status_code=302,
@@ -99,12 +110,9 @@ def redirect(suid):
             content_type="application/json",
             headers={"Location": long_url},
         )
-    except ClientError as e:
-        if e.response["Error"]["Code"] == "ConditionalCheckFailedException":
-            raise NotFoundError("Could not find short URL")
-        else:
-            logger.error(e)
-            raise BadRequestError("Bad UpdateItem request")
+    except IndexError as e:
+        raise NotFoundError("Could not find short URL")
+        logger.error(e)
     except Exception as e:
         logger.error(e)
         raise InternalServerError("Unexpected error during UpdateItem")
